259ffb95419f3e545cbbc52f3a73a0528cb35280
陈冲 <chenchong8806@gmail.com>
2018-06-01 14:55:04 +0800

    初始化

---
+++ app/src/main/java/com/foolchen/constraintlayout/samples/BarrierActivity.kt
@@ -0,0 +1,16 @@
+package com.foolchen.constraintlayout.samples
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author chenchong
+ * 2018/5/31
+ * 下午4:44
+ */
+class BarrierActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_barrier)
+  }
+}
---
+++ app/src/main/java/com/foolchen/constraintlayout/samples/ConstraintLayoutActivity.kt
@@ -0,0 +1,16 @@
+package com.foolchen.constraintlayout.samples
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author chenchong
+ * 2018/5/31
+ * 下午5:01
+ */
+class ConstraintLayoutActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_constraint_layout)
+  }
+}
